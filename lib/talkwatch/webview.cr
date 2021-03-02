b411e0e5debb8dfd40868b0efbf60e1fc5b3658b
frissyn <Ire.Raufu76@gmail.com>
2021-03-02 14:49:49 +0000

    ✨: initialize webview application

--- lib/talkwatch/webview.cr
+++ lib/talkwatch/webview.cr
@@ -3,7 +3,7 @@ require "kemal"
 module TalkWatch
     module Webview
         get "/" do
-            "TalkWatch running on version 0.1a"
+            render "views/index.ecr"
         end
 
         Kemal.config.port = 8080
