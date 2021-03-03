808106832999e13bd5aa5e45501b819c35c91231
frissyn <Ire.Raufu76@gmail.com>
2021-03-03 15:05:11 +0000

    ✨: miscellaneous changes to runner

--- main.cr
+++ main.cr
@@ -2,9 +2,8 @@ require "kemal"
 require "talkwatch"
 
 sid = ENV["connect.sid"]
+automod = TalkWatch::Moderator.new(sid)
 
-moderator = TalkWatch::Moderator.new(sid)
-
-puts "Running with user context: '#{moderator.user.username}'"
+puts "Running with user context: '#{automod.user.username}'"
 
 Kemal.run
